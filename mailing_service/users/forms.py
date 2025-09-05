32c1a8e2c9516f16e843d083492df81cd6c19837
tanec <tanec_991@mail.ru>
2025-09-05 23:25:18 +1000

    завершение проекта

--- mailing_service/users/forms.py
+++ mailing_service/users/forms.py
@@ -1,17 +1,48 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    UserChangeForm,
+    AuthenticationForm,
+)
 from .models import User
+
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
-        fields  =  ('email', 'password1', 'password2')
+        fields = ("email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
+
 
 class UserProfileForm(UserChangeForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'avatar', 'country')
+        fields = ("email", "first_name", "last_name", "phone", "avatar", "country")
 
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-        self.fields['password'].widget = forms.HiddenInput
+        super().__init__(*args, **kwargs)
+        self.fields["password"].widget = forms.HiddenInput()
+
+        for field_name, field in self.fields.items():
+            if field_name != "password":
+                field.widget.attrs["class"] = "form-control"
+
+
+#  ФОРМА ДЛЯ ВХОДА
+class UserLoginForm(AuthenticationForm):
+    username = forms.EmailField(
+        label="Email",
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Введите ваш email"}
+        ),
+    )
+    password = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Введите пароль"}
+        ),
+    )
